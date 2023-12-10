05d6ec7acf0edbf4e9a6305902323da95de1d225
thomasthaddeus <thaddeus.r.thomas@gmail.com>
2023-12-09 18:16:45 -0700

    Update dir_structure.md

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,9 +11,8 @@ android {
         applicationId = "com.example.programmingtools"
         minSdk = 29
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
-
+        versionCode = 2
+        versionName = "1.0.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
@@ -54,13 +53,13 @@ dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.1")
-    implementation(platform("androidx.compose:compose-bom:2023.10.01"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.zxing:core:3.5.2") // Add ZXing library dependency
+    implementation(platform("androidx.compose:compose-bom:2023.10.01"))
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
