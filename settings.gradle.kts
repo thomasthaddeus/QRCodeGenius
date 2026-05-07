cdc810550dae7938b197ca70cfa05be3eb04b6fc
thomasthaddeus <thaddeus.r.thomas@gmail.com>
2026-05-07 00:07:06 -0500

    fix dependecy issues

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,6 +5,7 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
 dependencyResolutionManagement {
     this.repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
